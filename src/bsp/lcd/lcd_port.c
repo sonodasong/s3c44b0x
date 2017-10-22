9d8e64f8e92733038fe7df3ca32a45b76f7a867f
sonodasong <sonodasong@gmail.com>
2017-10-22 15:20:45 +0800

    Set output pins to their default logic level before set them as output

--- src/bsp/lcd/lcd_port.c
+++ src/bsp/lcd/lcd_port.c
@@ -3,25 +3,30 @@
 void lcdPinInit(void)
 {
 	/* RESET */
+	LCD_RST_HIGH();
 	portControl(3, 0, 1);
 	portPullUp(3, 0, 0);
-	LCD_RST_HIGH();
+
 	/* CS */
+	LCD_CS_HIGH();
 	portControl(3, 2, 1);
 	portPullUp(3, 2, 0);
-	LCD_CS_HIGH();
+
 	/* RS */
+	LCD_RS_LOW();
 	portControl(3, 1, 1);
 	portPullUp(3, 1, 0);
-	LCD_RS_LOW();
+
 	/* RD */
+	LCD_RD_HIGH();
 	portControl(3, 3, 1);
 	portPullUp(3, 3, 0);
-	LCD_RD_HIGH();
+
 	/* WR */
+	LCD_WR_HIGH();
 	portControl(3, 5, 1);
 	portPullUp(3, 5, 0);
-	LCD_WR_HIGH();
+
 	/* Data */
 	rPCONC = 0x55555555;
 	rPUPC = 0x00;
